f5529b8858794a0ec1da4b18081e5a3e18736e6f
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-07-02 15:36:22 -0500

    display text for event descriptions saved in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@
 let currentDay = moment().format(`dddd, MMMM do`);
 $(`#currentDay`).text(currentDay);
 
-// Set background color of description based on whether the hour is in the past, present, or future
 let currentHour = moment().format(`H`);
 for (hour = 0; hour < 9; hour++) {
+  // Display text for event descriptions saved in local storage
+  $(`.description`).eq(hour).val(localStorage.getItem($(`.description`).eq(hour).prev().text()))
+  
+  // Set background color of event description based on whether the hour is in the past, present, or future  
   if (hour + 9 < currentHour) {
     $(`.description`).eq(hour).addClass(`.past`);
   }
@@ -16,7 +19,7 @@ for (hour = 0; hour < 9; hour++) {
   }
 }
 
-// If user clicks on save button, then save text in description to local storage
+// If user clicks on save button, then save text in event description to local storage
 $(`.saveBtn`).click(function(event) {
   let lsKey = event.target.previousElementSibling.previousElementSibling.textContent;
   let lsValue = event.target.previousElementSibling.value;
