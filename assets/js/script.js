0959aafbbe3c8fe6b673e974058a33e39af8bb57
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-07-02 11:43:18 -0500

    set description background color based on whether hour is in past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// Display current day in jumbotron
 let currentDay = moment().format(`dddd, MMMM do`);
 $(`#currentDay`).text(currentDay);
 
+// Set background color of description based on whether the hour is in the past, present, or future
+let currentHour = moment().format(`H`);
+for (hour = 0; hour < 9; hour++) {
+  if (hour + 9 < currentHour) {
+    // then description background color = gray
+    $(`.description`).eq(hour).addClass(`.past`);
+  }
+  if (hour + 9 == currentHour) {
+    // then description background color = red
+    $(`.description`).eq(hour).addClass(`.present`);
+  }
+  else {
+    // then description background color = green 
+    $(`.description`).eq(hour).addClass(`.future`);
+  }
+}
